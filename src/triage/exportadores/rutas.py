005c9c73f6cc19943fb639ecbc660d351df8ecf0
veracruzanisima-ti <veracruzanisimati@gmail.com>
2026-08-21 06:59:14 -0600

    docs(dif): actualiza condición de descarga emitible

--- src/triage/exportadores/rutas.py
+++ src/triage/exportadores/rutas.py
@@ -14,7 +14,7 @@ def descargar_dif(
     sesion: Sesion,
     _usuario: UsuarioActual,
 ) -> Response:
-    """Descarga un borrador DIF sólo cuando su base fiscal ya fue validada."""
+    """Descarga DIF sólo si precio final y tratamiento fiscal ya están confirmados."""
 
     try:
         exportacion = generar_exportacion_dif(sesion, cotizacion_id)
